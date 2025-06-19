7cbb98901339cd2c2f51566a2c5916cab3091e1d
Aayush Bharda <aayushbharda999@gmail.com>
2025-06-19 13:19:00 +0530

    Initial commit

--- vite.config.js
+++ vite.config.js
@@ -2,6 +2,6 @@ import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
 export default defineConfig({
-  base: '/College-Landing-Page-Somaiya/',
+  base: '/React-SignUp-And-Registration-Page/',
   plugins: [react()],
 })
